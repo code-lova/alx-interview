b5abe8e5cc64f060a775a71d4e13979874f5d718
code-lova <ebinuga@hotmail.com>
2023-10-20 00:34:07 +0100

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,37 @@
 #!/usr/bin/python3
-"""
-Function to pass log files
-"""
+'''a script that reads stdin line by line and computes metrics'''
+
+
 import sys
 
-
-def print_m(dictStatCode, total_file_size):
-    """
-    Args:
-        None
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dictStatCode.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
-
-
-total_file_size = 0
-code = 0
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
 counter = 0
-dictStatCode = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
 
 try:
     for line in sys.stdin:
-        current_line = line.split()  # trimming excessspaces
-        current_line = current_line[::-1]  # reversing the array
-
-        if len(current_line) > 2:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
             counter += 1
 
-            if counter <= 10:
-                total_file_size += int(current_line[0])  # save file size
-                code = current_line[1] 
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-                if (code in dictStatCode.keys()):
-                    dictStatCode[code] += 1
-
-            if (counter == 10):
-                print_m(dictStatCode, total_file_size)
-                counter = 0
+except Exception as err:
+    pass
 
 finally:
-    print_m(dictStatCode, total_file_size)
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
